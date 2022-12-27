9dbe482a8e51de25fbd2047d6f8bb48e576f81c3
TDF-PL-009 <118674359+TDF-PL-009@users.noreply.github.com>
2022-12-27 21:00:00 +0100

    Copy over cert files

--- c#/src/WOT.TAK.Client/Program.cs
+++ c#/src/WOT.TAK.Client/Program.cs
@@ -50,7 +50,7 @@ internal class Client
 
     public void DoWork()
     {
-        var connector = new CertificateConnector("212.160.99.185", "8089", null, @"~\mwolski1.p12", @"ZawszeBlisko2021");
+        var connector = new CertificateConnector("212.160.99.185", "8089", @"cot\responses", @"cot\cert\user.p12", @"atakatak");
         connector.Connect();
         connector.SendFile("cot/messages/msg1.cot");
         connector.SendFile("cot/messages/msg2.cot");

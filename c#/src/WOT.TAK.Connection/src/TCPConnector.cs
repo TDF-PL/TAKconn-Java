e3e2fec7c43db9b60e07dd76456c3f9436d95fc1
TDF-PL-009 <118674359+TDF-PL-009@users.noreply.github.com>
2022-12-27 19:27:57 +0100

    allow multiple calls for connect method

--- c#/src/WOT.TAK.Connection/src/TCPConnector.cs
+++ c#/src/WOT.TAK.Connection/src/TCPConnector.cs
@@ -52,7 +52,8 @@ namespace WOT.TAK.Connection
         {
             try
             {
-                _socket.Connect(_url, _port);
+                if(!_socket.Connected)
+                    _socket.Connect(_url, _port);
             }
             catch (Exception e)
             {

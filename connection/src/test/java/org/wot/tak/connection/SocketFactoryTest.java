70e3b5ec327040704440877a52b4ea4ca997410b
TDF-PL-008 <tdf-pl-008@pm.me>
2023-01-22 15:21:46 +0100

    tests for SocketFactory

--- connection/src/test/java/org/wot/tak/connection/SocketFactoryTest.java
+++ connection/src/test/java/org/wot/tak/connection/SocketFactoryTest.java
@@ -1,10 +1,58 @@
 package org.wot.tak.connection;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.IOException;
+
 public class SocketFactoryTest {
 
-    @Test
-    public void initializeTestSocketFactory_FactoryWithoutServerVerificationCreated() throws Exception {
-        //TODO Implement
+    public SocketFactory createFactory() {
+        SocketFactoryConfig config = new SocketFactoryConfig(
+                "test.p12",
+                "password",
+                "test.p12"
+                "password",
+                false,
+                );
+        return new SocketFactory(config);
     }
+
+    @Test
+    public void testCreateSSLSocket() throws Exception {
+        // given
+        SocketFactory factory = createFactory();
+        // when
+        Socket socket = factory.createSSLSocket("example.com", 443);\
+        // then
+        assertNotNull(socket);
+        assertTrue(socket instanceof SSLSocket);
+    }
+
+    @Test
+    public void testInitializeTestSocketFactory() throws Exception {
+        // given
+        SocketFactory factory = createFactory();
+
+        // when
+        factory.initializeTestSocketFactory();
+
+        // then
+        SSLSocketFactory sslFactory = factory.sslFactory;
+        assertNotNull(sslFactory);
+    }
+
+    @Test
+    public void testInitializeSocketFactory() throws Exception {
+        // given
+        SocketFactory factory = createFactory();
+
+        // when
+        factory.initializeSocketFactory();
+
+        // then
+        SSLSocketFactory sslFactory = factory.sslFactory;
+        assertNotNull(sslFactory);
+    }
+
 
 }

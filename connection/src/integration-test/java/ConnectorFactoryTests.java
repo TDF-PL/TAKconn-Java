395c79cc70f61178dce379af69c662d59ad87a5c
TDF-PL-038 <145844128+TDF-PL-038@users.noreply.github.com>
2024-01-28 10:06:11 +0100

    Add Integration Test for creating SSL Connector from ConnectorFactory.

---
+++ connection/src/integration-test/java/ConnectorFactoryTests.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test;
+import org.wot.tak.common.Port;
+import org.wot.tak.common.Url;
+import org.wot.tak.connection.UID;
+import org.wot.tak.connection.configuration.AuthenticationConfig;
+import org.wot.tak.connection.configuration.ConnectorFactory;
+import org.wot.tak.connection.configuration.ConnectorFactoryConfig;
+import org.wot.tak.connection.connectors.Receiver;
+import org.wot.tak.connection.protocol.ProtocolVersion;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.wot.tak.connection.messages.ClientProtobufMessage.announcement;
+
+class ConnectorFactoryTests {
+
+    @Test
+    void Can_create_SSL_connector_using_connector_factory() throws Exception {
+
+        var config = new ConnectorFactoryConfig(
+                Url.of("tak-dev.1gs20.net"),
+                Port.of(8089),
+                new AuthenticationConfig(
+                        "",
+                        "",
+                        "cert-test/truststore-int-ca.p12",
+                        "atakatak",
+                        "cert-test/charlie.p12",
+                        "atakatak",
+                        true));
+
+        var connectorFactory = new ConnectorFactory(config);
+
+        try (var senderConnector = connectorFactory.getSSLConnector();
+             var receiverConnector = connectorFactory.getSSLConnector()) {
+
+            senderConnector.connect(new Receiver());
+
+            var receiver = new Receiver();
+            receiverConnector.connect(receiver);
+
+            await().atMost(5, TimeUnit.SECONDS)
+                    .untilAsserted(
+                            () -> {
+                                assertThat(senderConnector.getProtocolVersion())
+                                        .isEqualTo(ProtocolVersion.PROTOBUF);
+
+                                assertThat(receiverConnector.getProtocolVersion())
+                                        .isEqualTo(ProtocolVersion.PROTOBUF);
+                            });
+
+            var message = announcement(UID.random(), "sender");
+            senderConnector.send(message);
+
+            await().atMost(5, TimeUnit.SECONDS).untilAsserted(receiver.received(message));
+        }
+    }
+}
--- connection/src/test/java/org/wot/tak/connection/connectors/Receiver.java
+++ connection/src/test/java/org/wot/tak/connection/connectors/Receiver.java
@@ -1,14 +1,20 @@
 package org.wot.tak.connection.connectors;
 
 import lombok.Getter;
+import org.awaitility.core.ThrowingRunnable;
 import org.wot.tak.connection.configuration.MessageReceiver;
+import org.wot.tak.connection.protocol.protobuf.DetailOuterClass;
 import org.wot.tak.connection.protocol.protobuf.Takmessage;
 import org.wot.tak.connection.protocol.xml.Event;
 
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @Getter
-class Receiver implements MessageReceiver {
+public class Receiver implements MessageReceiver {
 
     private final CopyOnWriteArrayList<Event> xmlMessages = new CopyOnWriteArrayList<>();
 
@@ -22,5 +28,19 @@ class Receiver implements MessageReceiver {
     @Override
     public void receiveByProtobufProtocol(Takmessage.TakMessage event) {
         protobufMessages.add(event);
+    }
+
+    public ThrowingRunnable received(Takmessage.TakMessage message) {
+        return () -> {
+            var contact = Optional.ofNullable(message.getCotEvent().getDetail().getContact());
+
+            var receivedContacts = protobufMessages.stream()
+                    .map(Takmessage.TakMessage::getCotEvent)
+                    .map(event -> Optional.ofNullable(event.getDetail()))
+                    .map(detail -> detail.map(DetailOuterClass.Detail::getContact))
+                    .collect(Collectors.toList());
+
+            assertThat(receivedContacts).contains(contact);
+        };
     }
 }
